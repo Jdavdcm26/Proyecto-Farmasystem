9d51b0c69c9a5c274e32dc80e37b1929ed8466f8
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-06-04 15:09:36 -0500

    feat: crear interfaz AdminService

---
+++ src/main/java/com/farmaciaproyecto/service/AdminService.java
@@ -0,0 +1,7 @@
+package com.farmaciaproyecto.service;
+
+import com.farmaciaproyecto.dto.response.DashboardAdminResponseDTO;
+
+public interface AdminService {
+    DashboardAdminResponseDTO obtenerDashboard();
+}
