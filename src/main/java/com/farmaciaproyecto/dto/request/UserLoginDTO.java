b73b1ee9fb215156be621b99cc2efd21448e8a57
Andry Carolay Camacho Martinez <acarolaycamacho@unicesar.edu.co>
2026-06-04 13:33:13 -0500

    feat: crear DTO UserLoginDTO con validaciones de usuario y contraseña

---
+++ src/main/java/com/farmaciaproyecto/dto/request/UserLoginDTO.java
@@ -0,0 +1,20 @@
+package com.farmaciaproyecto.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDTO {
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    private String username;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    private String password;
+}
