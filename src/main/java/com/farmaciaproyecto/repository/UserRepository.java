05540f5ca3dc10488e5e8f422f86ee1939b495ea
Andry Carolay Camacho Martinez <acarolaycamacho@unicesar.edu.co>
2026-06-04 13:04:25 -0500

    feat: crear repositorio UserRepository con consultas personalizadas JPA

---
+++ src/main/java/com/farmaciaproyecto/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.farmaciaproyecto.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import com.farmaciaproyecto.model.User;
+import org.springframework.data.repository.query.Param;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    Optional<User> findByRol(String rol);
+    boolean existsByRol(String rol);
+    @Query("SELECT u FROM User u WHERE u.username = :username AND u.password = :password")
+    Optional<User> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+}
